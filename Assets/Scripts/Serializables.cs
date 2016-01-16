4a35accf2fc1217eb5591faa6f5de5177c2b2c3d
Eudaimonium <eudaimonium@gmail.com>
2016-01-16 10:43:29 +0100

    Article 1 finish point example

---
+++ Assets/Scripts/Serializables.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+//TUTORIAL
+[Serializable]
+public class PlayerStatistics
+{
+    public int SceneID;
+    public float PositionX, PositionY, PositionZ;
+
+    public float HP;
+    public float Ammo;
+    public float XP;
+}
+
+
